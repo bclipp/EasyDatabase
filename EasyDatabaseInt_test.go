384f3512aece72ea65750692a2a9e0abe6f664eb
blipp <bclipp770@gmail.com>
2020-07-22 19:46:10 -0400

    fixing

--- EasyDatabaseInt_test.go
+++ EasyDatabaseInt_test.go
@@ -4,6 +4,7 @@ export USERNAME=testing1234
 export PASSWORD=testing1234
 export DB_IP_ADDRESS=127.0.0.1
 export INT_TEST=true
+DATABASE=testing1234;PASSWORD=testing1234;DB_IP_ADDRESS=testing1234;INT_TEST=true;USERNAME=root
 */
 package EasyDatabase
 
@@ -13,6 +14,58 @@ import (
 	"os"
 	"testing"
 )
+
+
+func TestPostgreSQL_ConnectDisconnect(t *testing.T){
+	config := GetVariables()
+	var pg = PostgreSQL{
+		IPAddress:        config["postgresIP"],
+		PostgresPassword: config["postgresPassword"],
+		PostgresUser:     config["postgresUser"],
+		PostgresDB:       config["postgresDB"],
+	}
+	err := pg.Connect();if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer pg.Disconnect()
+}
+
+func TestPostgreSQL_SendQuery(t *testing.T){
+	config := GetVariables()
+	var pg = PostgreSQL{
+		IPAddress:        config["postgresIP"],
+		PostgresPassword: config["postgresPassword"],
+		PostgresUser:     config["postgresUser"],
+		PostgresDB:       config["postgresDB"],
+	}
+	err := pg.Connect();if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer pg.Disconnect()
+	_,err = pg.SendQuery("SELECT * FROM customers;");if err != nil {
+		fmt.Print(err.Error())
+	}
+}
+
+func TestPostgreSQL_sendQueryReturnData(t *testing.T){
+	//config := GetVariables()
+	var pg = PostgreSQL{
+		IPAddress:        "127.0.0.1",
+		PostgresPassword: "testing1234",
+		PostgresUser:     "root",
+		PostgresDB:       "testing1234",
+	}
+	err := pg.Connect();if err != nil {
+		fmt.Print(err.Error())
+	}
+	// Row is used to hold a row of data from a table in the DB
+	// only common data is used and needed.
+
+	defer pg.Disconnect()
+	_,err = pg.sendQueryReturnData("SELECT * FROM customers;",processTable);if err != nil {
+		fmt.Print(err.Error())
+	}
+}
 
 // get_variables are used to hold environmental variables read by the app
 func GetVariables() map[string]string {
@@ -70,53 +123,3 @@ func processTable(rows *sql.Rows) ([]struct{},error) {
 }
 
 
-func TestPostgreSQL_ConnectDisconnect(t *testing.T){
-	config := GetVariables()
-	var pg = PostgreSQL{
-		IPAddress:        config["postgresIP"],
-		PostgresPassword: config["postgresPassword"],
-		PostgresUser:     config["postgresUser"],
-		PostgresDB:       config["postgresDB"],
-	}
-	err := pg.Connect();if err != nil {
-		fmt.Print(err.Error())
-	}
-	defer pg.Disconnect()
-}
-
-func TestPostgreSQL_SendQuery(t *testing.T){
-	config := GetVariables()
-	var pg = PostgreSQL{
-		IPAddress:        config["postgresIP"],
-		PostgresPassword: config["postgresPassword"],
-		PostgresUser:     config["postgresUser"],
-		PostgresDB:       config["postgresDB"],
-	}
-	err := pg.Connect();if err != nil {
-		fmt.Print(err.Error())
-	}
-	defer pg.Disconnect()
-	_,err = pg.SendQuery("SELECT * FROM customers;");if err != nil {
-		fmt.Print(err.Error())
-	}
-}
-
-func TestPostgreSQL_sendQueryReturnData(t *testing.T){
-	config := GetVariables()
-	var pg = PostgreSQL{
-		IPAddress:        config["postgresIP"],
-		PostgresPassword: config["postgresPassword"],
-		PostgresUser:     config["postgresUser"],
-		PostgresDB:       config["postgresDB"],
-	}
-	err := pg.Connect();if err != nil {
-		fmt.Print(err.Error())
-	}
-	// Row is used to hold a row of data from a table in the DB
-	// only common data is used and needed.
-
-	defer pg.Disconnect()
-	_,err = pg.sendQueryReturnData("SELECT * FROM customers;",processTable);if err != nil {
-		fmt.Print(err.Error())
-	}
-}
